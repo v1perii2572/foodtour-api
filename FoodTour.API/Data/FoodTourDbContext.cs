5e2d601c3603cadbc2e72b05caca9392cbea856b
v1perii2572 <v1perii2570202@gmail.com>
2025-05-31 21:12:07 +0700

    Fix Dockerfile and connection for Railway deploy

--- FoodTour.API/Data/FoodTourDbContext.cs
+++ FoodTour.API/Data/FoodTourDbContext.cs
@@ -3,164 +3,165 @@ using System.Collections.Generic;
 using FoodTour.API.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace FoodTour.API.Data;
-
-public partial class FoodTourDbContext : DbContext
+namespace FoodTour.API.Data
 {
-    public FoodTourDbContext()
+    public partial class FoodTourDbContext : DbContext
     {
+        public FoodTourDbContext()
+        {
+        }
+
+        public FoodTourDbContext(DbContextOptions<FoodTourDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ChatMessage> ChatMessages { get; set; }
+        public virtual DbSet<ChatSession> ChatSessions { get; set; }
+        public virtual DbSet<Feedback> Feedbacks { get; set; }
+        public virtual DbSet<SavedRoute> SavedRoutes { get; set; }
+        public virtual DbSet<SavedRoutePlace> SavedRoutePlaces { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<PaymentTransaction> PaymentTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatMessage>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__ChatMess__3214EC07A873D6F9");
+
+                entity.Property(e => e.Role)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Timestamp)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp");
+
+                entity.HasOne(d => d.Session).WithMany(p => p.ChatMessages)
+                    .HasForeignKey(d => d.SessionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__ChatMessa__Sessi__412EB0B6");
+            });
+
+            modelBuilder.Entity<ChatSession>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__ChatSess__3214EC079EDCB152");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.StartedAt)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp");
+
+                entity.Property(e => e.Status)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasDefaultValue("InProgress");
+
+                entity.HasOne(d => d.User).WithMany(p => p.ChatSessions)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__ChatSessi__UserI__3D5E1FD2");
+            });
+
+            modelBuilder.Entity<Feedback>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Feedback__3214EC0793A9AD32");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp");
+
+                entity.Property(e => e.RelatedPlaceName).HasMaxLength(255);
+
+                entity.HasOne(d => d.Session).WithMany(p => p.Feedbacks)
+                    .HasForeignKey(d => d.SessionId)
+                    .HasConstraintName("FK__Feedbacks__Sessi__44FF419A");
+
+                entity.HasOne(d => d.User).WithMany(p => p.Feedbacks)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__Feedbacks__UserI__45F365D3");
+            });
+
+            modelBuilder.Entity<SavedRoute>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__SavedRou__3214EC0735231D1C");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                entity.Property(e => e.Name).HasMaxLength(255);
+
+                entity.Property(e => e.SavedAt)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp");
+
+                entity.HasOne(d => d.Session).WithMany(p => p.SavedRoutes)
+                    .HasForeignKey(d => d.SessionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__SavedRout__Sessi__49C3F6B7");
+
+                entity.HasOne(d => d.User).WithMany(p => p.SavedRoutes)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__SavedRout__UserI__4AB81AF0");
+            });
+
+            modelBuilder.Entity<SavedRoutePlace>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__SavedRou__3214EC07757FEEB3");
+
+                entity.Property(e => e.Address).HasMaxLength(255);
+                entity.Property(e => e.Name).HasMaxLength(255);
+                entity.Property(e => e.Role)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+                entity.Property(e => e.TimeSlot)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Route).WithMany(p => p.SavedRoutePlaces)
+                    .HasForeignKey(d => d.RouteId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__SavedRout__Route__4D94879B");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07BF59CE01");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+                entity.Property(e => e.Email).HasMaxLength(255);
+                entity.Property(e => e.PasswordHash).HasMaxLength(255);
+                entity.Property(e => e.Role)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasDefaultValue("Free");
+
+                entity.Property(e => e.SubscriptionDate)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp");
+            });
+
+            modelBuilder.Entity<PaymentTransaction>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__PaymentT__3214EC072029B684");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp");
+
+                entity.Property(e => e.Message).HasMaxLength(500);
+                entity.Property(e => e.OrderId).HasMaxLength(100);
+                entity.Property(e => e.RequestId).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    public FoodTourDbContext(DbContextOptions<FoodTourDbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<ChatMessage> ChatMessages { get; set; }
-
-    public virtual DbSet<ChatSession> ChatSessions { get; set; }
-
-    public virtual DbSet<Feedback> Feedbacks { get; set; }
-
-    public virtual DbSet<SavedRoute> SavedRoutes { get; set; }
-
-    public virtual DbSet<SavedRoutePlace> SavedRoutePlaces { get; set; }
-
-    public virtual DbSet<User> Users { get; set; }
-
-    public virtual DbSet<PaymentTransaction> PaymentTransactions { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<ChatMessage>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__ChatMess__3214EC07A873D6F9");
-
-            entity.Property(e => e.Role)
-                .HasMaxLength(20)
-                .IsUnicode(false);
-            entity.Property(e => e.Timestamp)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-
-            entity.HasOne(d => d.Session).WithMany(p => p.ChatMessages)
-                .HasForeignKey(d => d.SessionId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__ChatMessa__Sessi__412EB0B6");
-        });
-
-        modelBuilder.Entity<ChatSession>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__ChatSess__3214EC079EDCB152");
-
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.StartedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-            entity.Property(e => e.Status)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasDefaultValue("InProgress");
-
-            entity.HasOne(d => d.User).WithMany(p => p.ChatSessions)
-                .HasForeignKey(d => d.UserId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__ChatSessi__UserI__3D5E1FD2");
-        });
-
-        modelBuilder.Entity<Feedback>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Feedback__3214EC0793A9AD32");
-
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-            entity.Property(e => e.RelatedPlaceName).HasMaxLength(255);
-
-            entity.HasOne(d => d.Session).WithMany(p => p.Feedbacks)
-                .HasForeignKey(d => d.SessionId)
-                .HasConstraintName("FK__Feedbacks__Sessi__44FF419A");
-
-            entity.HasOne(d => d.User).WithMany(p => p.Feedbacks)
-                .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK__Feedbacks__UserI__45F365D3");
-        });
-
-        modelBuilder.Entity<SavedRoute>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__SavedRou__3214EC0735231D1C");
-
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.Name).HasMaxLength(255);
-            entity.Property(e => e.SavedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-
-            entity.HasOne(d => d.Session).WithMany(p => p.SavedRoutes)
-                .HasForeignKey(d => d.SessionId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__SavedRout__Sessi__49C3F6B7");
-
-            entity.HasOne(d => d.User).WithMany(p => p.SavedRoutes)
-                .HasForeignKey(d => d.UserId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__SavedRout__UserI__4AB81AF0");
-        });
-
-        modelBuilder.Entity<SavedRoutePlace>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__SavedRou__3214EC07757FEEB3");
-
-            entity.Property(e => e.Address).HasMaxLength(255);
-            entity.Property(e => e.Name).HasMaxLength(255);
-            entity.Property(e => e.Role)
-                .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.TimeSlot)
-                .HasMaxLength(50)
-                .IsUnicode(false);
-
-            entity.HasOne(d => d.Route).WithMany(p => p.SavedRoutePlaces)
-                .HasForeignKey(d => d.RouteId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__SavedRout__Route__4D94879B");
-        });
-
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07BF59CE01");
-
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.Email).HasMaxLength(255);
-            entity.Property(e => e.PasswordHash).HasMaxLength(255);
-            entity.Property(e => e.Role)
-                .HasMaxLength(20)
-                .IsUnicode(false)
-                .HasDefaultValue("Free");
-            entity.Property(e => e.SubscriptionDate)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-        });
-
-        modelBuilder.Entity<PaymentTransaction>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__PaymentT__3214EC072029B684");
-
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("(getdate())")
-                .HasColumnType("datetime");
-            entity.Property(e => e.Message).HasMaxLength(500);
-            entity.Property(e => e.OrderId).HasMaxLength(100);
-            entity.Property(e => e.RequestId).HasMaxLength(100);
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ FoodTour.API/Migrations/20250531140913_InitPostgres1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FoodTour.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitPostgres1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderId = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    RequestId = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    ResultCode = table.Column<int>(type: "integer", nullable: false),
+                    Message = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__PaymentT__3214EC072029B684", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    PasswordHash = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Role = table.Column<string>(type: "character varying(20)", unicode: false, maxLength: 20, nullable: true, defaultValue: "Free"),
+                    SubscriptionDate = table.Column<DateTime>(type: "timestamp", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    DislikedFoods = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Users__3214EC07BF59CE01", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "timestamp", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    Status = table.Column<string>(type: "character varying(50)", unicode: false, maxLength: 50, nullable: true, defaultValue: "InProgress")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__ChatSess__3214EC079EDCB152", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__ChatSessi__UserI__3D5E1FD2",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Role = table.Column<string>(type: "character varying(20)", unicode: false, maxLength: 20, nullable: true),
+                    Message = table.Column<string>(type: "text", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "timestamp", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__ChatMess__3214EC07A873D6F9", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__ChatMessa__Sessi__412EB0B6",
+                        column: x => x.SessionId,
+                        principalTable: "ChatSessions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    RelatedPlaceName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Comment = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Feedback__3214EC0793A9AD32", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Feedbacks__Sessi__44FF419A",
+                        column: x => x.SessionId,
+                        principalTable: "ChatSessions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Feedbacks__UserI__45F365D3",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SavedRoutes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    SavedAt = table.Column<DateTime>(type: "timestamp", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__SavedRou__3214EC0735231D1C", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__SavedRout__Sessi__49C3F6B7",
+                        column: x => x.SessionId,
+                        principalTable: "ChatSessions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__SavedRout__UserI__4AB81AF0",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SavedRoutePlaces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RouteId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Address = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Lat = table.Column<double>(type: "double precision", nullable: true),
+                    Lng = table.Column<double>(type: "double precision", nullable: true),
+                    Sequence = table.Column<int>(type: "integer", nullable: true),
+                    TimeSlot = table.Column<string>(type: "character varying(50)", unicode: false, maxLength: 50, nullable: true),
+                    Role = table.Column<string>(type: "character varying(50)", unicode: false, maxLength: 50, nullable: true),
+                    Note = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__SavedRou__3214EC07757FEEB3", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__SavedRout__Route__4D94879B",
+                        column: x => x.RouteId,
+                        principalTable: "SavedRoutes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_SessionId",
+                table: "ChatMessages",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatSessions_UserId",
+                table: "ChatSessions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_SessionId",
+                table: "Feedbacks",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_UserId",
+                table: "Feedbacks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedRoutePlaces_RouteId",
+                table: "SavedRoutePlaces",
+                column: "RouteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedRoutes_SessionId",
+                table: "SavedRoutes",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedRoutes_UserId",
+                table: "SavedRoutes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatMessages");
+
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
+            migrationBuilder.DropTable(
+                name: "PaymentTransactions");
+
+            migrationBuilder.DropTable(
+                name: "SavedRoutePlaces");
+
+            migrationBuilder.DropTable(
+                name: "SavedRoutes");
+
+            migrationBuilder.DropTable(
+                name: "ChatSessions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
